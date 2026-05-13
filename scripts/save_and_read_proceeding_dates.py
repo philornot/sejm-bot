dd1728d12c934d2b942f75274638d8b76e481fcf
philornot <philornot@users.noreply.github.com>
2026-05-13 12:04:06 +0200

    merge scripts regarding writing and reading proceeding dates from file

--- scripts/save_and_read_proceeding_dates.py
+++ scripts/save_and_read_proceeding_dates.py
@@ -45,5 +45,20 @@ def save_proceeding_dates_to_file(term):
         return dates
 
 
+def read_proceeding_dates_from_file(term):
+    """Reads proceeding dates for a given Sejm term from a local JSON file.
+
+    Args:
+        term: Sejm term number (kadencja).
+
+    Returns:
+        A list of date strings loaded from the file.
+    """
+    file_path = f'../proceeding_dates/term{term}.json'
+    with open(file_path, 'r') as json_file:
+        dates = json.load(json_file)
+    return dates
+
+
 for term in range(7, 11):
     save_proceeding_dates_to_file(term)
