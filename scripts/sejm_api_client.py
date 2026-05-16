5fc25b9672c08948febe9530e6139652737ca7bc
philornot <philornot@users.noreply.github.com>
2026-05-16 12:51:38 +0200

    refactor transcript parsing and update file path structure

--- scripts/sejm_api_client.py
+++ scripts/sejm_api_client.py
@@ -34,23 +34,16 @@ def get_speech_transcripts(proceeding_number, date, transcript_number):
     parsed_transcript = BeautifulSoup(html_transcript, 'html.parser')
 
     speakers = parsed_transcript.find_all('h2', class_='mowca')
-    if len(speakers) == 1:
-        speaker = speakers[0]
-        transcript = []
-        transcript_part = speaker.find_next_sibling()
-        while transcript_part and transcript_part.name != 'br':
-            if transcript_part.name == 'p':
-                transcript.append(transcript_part.get_text(" ", strip=True))
-            transcript_part = transcript_part.find_next_sibling()
+    if len(speakers) != 1:
+        print(f'found multiple speakers in one transcript!')
+        raise ValueError
 
-        transcripts = [format_paragraphs(speaker)]
-    else:
-        transcripts = [format_paragraphs(speaker) for speaker in speakers]
-    return transcripts
+    speaker = speakers[0]
+    return format_paragraphs(speaker)
 
 
 def write_speech_to_json(proceeding_number, date, transcript_number):
-    base_dir = f'../speeches/{date}/proceeding{proceeding_number}'
+    base_dir = f'../speeches_term{TERM}/proceeding{proceeding_number}/{date}'
     file_path = f'{base_dir}/transcript{transcript_number}.json'
     pathlib.Path(base_dir).mkdir(parents=True, exist_ok=True)
 
@@ -64,7 +57,7 @@ def write_speech_to_json(proceeding_number, date, transcript_number):
 
 
 def read_speeches_from_json(proceeding_number, date, transcript_number):
-    file_path = f'../speeches/{date}/proceeding{proceeding_number}/transcript{transcript_number}.json'
+    file_path = f'../speeches_term{TERM}/proceeding{proceeding_number}/{date}/transcript{transcript_number}.json'
     with open(file_path, 'r') as json_file:
         speeches = json.load(json_file)
     return speeches
@@ -75,12 +68,15 @@ if __name__ == "__main__":
     for proceeding_number, dates in proceeding_dates.items():
         for date in dates:
             print(f'trying date {date}')
-            try:
-                transcript_number = 1
-                while True:
-                    print(f'writing transcript {transcript_number} from proceeding {proceeding_number} from date {date} ')
+            transcript_number = 1
+            while True:
+                try:
+                    print(f'writing transcript {transcript_number} from proceeding {proceeding_number} from date {date}')
                     write_speech_to_json(proceeding_number, date, transcript_number)
                     transcript_number += 1
-            except Exception as e:
-                print(f'sth went wrong with writing transcript from {date} (proceeding {proceeding_number}):\n{e}\n')
-                continue
+                except RuntimeError:
+                    print(f'no more transcripts for date {date} (last: {transcript_number})\n')
+                    break
+                except Exception as e:
+                    print(f'sth went wrong with writing transcript from {date} (proceeding {proceeding_number}):\n{e}\n')
+                    break
